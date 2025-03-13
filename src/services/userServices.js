dc297b3ec8a6f02a0f32b19e73342cd0e5a4c668
Cristian Medina <ccfranm17@gmail.com>
2025-03-13 16:02:15 -0500

    Refactor: Update userServices.js error lint

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -1,7 +1,7 @@
 import { api_root } from './root'
 
 const logginUser = (data) => {
-  return fetch(`${api_root}/session`, { method: 'POST', headers: { 'Content-Type': 'application/json; charset=UTF-8' }, body: JSON.stringify(data) , mode: 'no-cors'})
+  return fetch(`${api_root}/session`, { method: 'POST', headers: { 'Content-Type': 'application/json; charset=UTF-8' }, body: JSON.stringify(data), mode: 'no-cors' })
 }
 
 const validateUser = (data) => {
